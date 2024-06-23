e5b21739f3d2ebc72f9a3382ce9881be8fe1563c
Aitana <aitana.ses@gmail.com>
2024-06-23 18:17:33 +0200

    modified pages

--- pages/Mapa.py
+++ pages/Mapa.py
@@ -1,30 +1,53 @@
+from streamlit_folium import folium_static
 import streamlit as st
-import streamlit.components.v1 as components
+import geopandas as gpd
+import folium
 
 st.set_page_config(page_title="Mapa", page_icon="🗺️")
-
 st.markdown("# Mapa de ubicaciones")
 st.sidebar.header("Mapa")
 st.write("""Este mapa de València muestra las ubicaciones de los diferentes contenedores y papeleras repartidos por la ciudad. Los datos han sido
-         obtenidos desde el [`Portal de dades obertes de València`](https://valencia.opendatasoft.com/pages/home/) """)
+         obtenidos desde el [Portal de dades obertes de València](https://valencia.opendatasoft.com/pages/home/)""")
 
-html_file_path = "mapa_contenedores_valencia.html"
+# Leer el archivo GeoJSON
+gdf = gpd.read_file('./processed_data/reciclatge.geojson')
 
-try:
-    with open(html_file_path, 'r', encoding='utf-8') as html_file:
-        html_content = html_file.read()
-        components.html(html_content, height=600, scrolling=True)
-except FileNotFoundError:
-    st.error("No se encuentra la página.")
+# Crear un mapa centrado en Valencia
+valencia_coords = [39.4699, -0.3763]
+mapa = folium.Map(location=valencia_coords, zoom_start=13)
 
+# Diccionario para colores e iconos por tipo
+iconos = {
+    'Aceite': ('green', 'oil-can'),
+    'Envases ligeros': ('orange', 'recycle'),
+    'Organico': ('lightgreen', 'leaf'),
+    'Papel / carton': ('blue', 'file-text'),
+    'Papeleras': ('purple', 'trash'),
+    'Pilas': ('gray', 'battery-quarter'),
+    'Resto': ('darkred', 'globe'),
+    'Ropa': ('pink', 'tshirt'),
+    'Vidrio': ('green', 'glass-martini')
+}
 
-html_file_path_2 = "selecciona_contenedores.html"
+# Añadir contenedores al mapa
+for idx, row in gdf.iterrows():
+    tipo = row['tipo'].capitalize()
+    location = [row['lat'], row['long']]
+    
+    # Obtener el color y el icono correspondiente al tipo
+    if tipo in iconos:
+        color, icon = iconos[tipo]
+    else:
+        color, icon = 'gray', 'info-sign'
+    
+    folium.Marker(
+        location=location,
+        popup=f"Tipo: {tipo}",
+        icon=folium.Icon(color=color, icon=icon, prefix='fa')
+    ).add_to(mapa)
 
-try:
-    with open(html_file_path_2, 'r', encoding='utf-8') as html_file:
-        html_content = html_file.read()
-        components.html(html_content, height=600, scrolling=True)
-except FileNotFoundError:
-    st.error("No se encuentra la página.")
+# Añadir el control de capas al mapa
+folium.LayerControl(collapsed=False).add_to(mapa)
 
-st.button("Volver a cargar")
+# Mostrar el mapa en Streamlit
+folium_static(mapa)
