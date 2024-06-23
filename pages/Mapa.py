03228135b929465a956b41baa24e2b5457daa8b9
Aitana <aitana.ses@gmail.com>
2024-06-23 19:03:53 +0200

    add map

--- pages/Mapa.py
+++ pages/Mapa.py
@@ -4,6 +4,7 @@ import geopandas as gpd
 import folium
 import streamlit.components.v1 as components
 
+
 st.set_page_config(page_title="Mapa", page_icon="🗺️")
 st.markdown("# Mapa de ubicaciones")
 st.sidebar.header("Mapa")
@@ -11,6 +12,17 @@ st.write("""Este mapa de València muestra las ubicaciones de los diferentes con
          obtenidos desde el [Portal de dades obertes de València](https://valencia.opendatasoft.com/pages/home/)""")
 
 html_file_path = "./processed_data/mapa_contenedores_valencia.html"
+
+try:
+    with open(html_file_path, 'r', encoding='utf-8') as html_file:
+        html_content = html_file.read()
+        components.html(html_content, height=600, scrolling=True)
+except FileNotFoundError:
+    st.error("No se encuentra la página.")
+
+st.markdown("# Mapa de ubicaciones interactivo")
+
+html_file_path = "./processed_data/selecciona_contenedores.html"
 
 try:
     with open(html_file_path, 'r', encoding='utf-8') as html_file:
