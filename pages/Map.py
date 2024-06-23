d79b97317bea2f37178a8c553203463a3ca0475f
Aitana <aitana.ses@gmail.com>
2024-06-23 13:48:11 +0200

    added pages

--- pages/Map.py
+++ pages/Map.py
@@ -3,6 +3,7 @@ import pandas as pd
 import pydeck as pdk
 from urllib.error import URLError
 
+
 st.set_page_config(page_title="Mapping Demo", page_icon="🌍")
 
 st.markdown("# Mapping Demo")
--- pages/Mapa.py
+++ pages/Mapa.py
@@ -1,33 +1,21 @@
 import streamlit as st
-import time
-import numpy as np
+import streamlit.components.v1 as components
 
-st.set_page_config(page_title="Plotting Demo", page_icon="📈")
+st.set_page_config(page_title="Mapa", page_icon="📈")
 
-st.markdown("# Plotting Demo")
-st.sidebar.header("Plotting Demo")
-st.write(
-    """This demo illustrates a combination of plotting and animation with
-Streamlit. We're generating a bunch of random numbers in a loop for around
-5 seconds. Enjoy!"""
-)
+st.markdown("# Mapa d'ubicacions")
+st.sidebar.header("Mapa")
+st.write("Aquest mapa de València mostra les ubicacions dels diferents contenedors i papereres distribuïdes a València.")
 
-progress_bar = st.sidebar.progress(0)
-status_text = st.sidebar.empty()
-last_rows = np.random.randn(1, 1)
-chart = st.line_chart(last_rows)
+# Leer el archivo HTML y cargarlo en la aplicación de Streamlit
+html_file_path = 'mapa_contenedores_valencia.html'
 
-for i in range(1, 101):
-    new_rows = last_rows[-1, :] + np.random.randn(5, 1).cumsum(axis=0)
-    status_text.text("%i%% Complete" % i)
-    chart.add_rows(new_rows)
-    progress_bar.progress(i)
-    last_rows = new_rows
-    time.sleep(0.05)
+# Asegurarse de que el archivo HTML exista
+try:
+    with open(html_file_path, 'r', encoding='utf-8') as html_file:
+        html_content = html_file.read()
+        components.html(html_content, height=600, scrolling=True)
+except FileNotFoundError:
+    st.error("El archivo HTML del mapa no se encuentra. Por favor, verifica la ruta y asegúrate de que el archivo haya sido generado correctamente.")
 
-progress_bar.empty()
-
-# Streamlit widgets automatically run the script from top to bottom. Since
-# this button is not connected to any other logic, it just causes a plain
-# rerun.
 st.button("Re-run")
