d1905d8074cae1e1dd3a22924debcff504692a07
Aitana <aitana.ses@gmail.com>
2024-06-24 23:56:39 +0200

    added contacta

--- pages/Contacto.py
+++ pages/Contacto.py
@@ -1,13 +1,16 @@
 import streamlit as st
 
-
+# Configuración de la página
 st.set_page_config(page_title="Contacta", page_icon="📞")
 
 st.markdown("# Contacta")
 st.sidebar.header("Contacta")
-st.write("¿Has visto un contenedor que requiera de atención? !Ponte en contacto! De esta forma podremos optimizar los esfuerzos municipales.")
+st.write("¿Has visto un contenedor que requiera de atención? ¡Ponte en contacto! De esta forma podremos optimizar los esfuerzos municipales.")
 
+# Define la dirección de correo
+email_address = "tucorreo@example.com"  # Reemplaza esto con la dirección de correo real
 
+# Formulario de contacto
 contact_form = f"""
 <form action="https://formsubmit.co/{email_address}" method="POST">
     <input type="hidden" name="_captcha" value="false">
@@ -18,3 +21,5 @@ contact_form = f"""
     <button type="submit">Enviar</button>
 </form>
 """
+
+st.markdown(contact_form, unsafe_allow_html=True)
